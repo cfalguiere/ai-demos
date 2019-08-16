7d90ccbd3c35f7e72efcc6811331243e057cfe00
Claude Falguiere <cfalguiere@gmail.com>
2019-08-16 18:33:46 +0200

    added h2o run script + fixed h2o install

--- env-setup/install-tools.sh
+++ env-setup/install-tools.sh
@@ -6,10 +6,15 @@ echo "starting install-tools $( date )"
 set -eu
 trap "{ echo 'install failed' ; exit 255; }" SIGINT SIGTERM ERR
 
+TRACKING_DIR="/var/log/aidemos/setup"
+mkdir -p "${TRACKING_DIR}"
+TMP_DIR="/tmp/aidemos"
+mkdir -p "${TMP_DIR}"
+
 mkdir -p /opt/
 
 # anaconda
-[[ -d /opt/miniconda ]] || {
+[[ -f "${TRACKING_DIR}/.anaconda" ]] || {
   echo "=== installing anaconda"
 
   ANACONDA_VERSION="Anaconda3-2019.07-Linux-x86_64.sh"
@@ -20,35 +25,50 @@ mkdir -p /opt/
   /opt/miniconda/bin/conda update -y conda
   cp env-setup/admin-condarc /opt/miniconda/.condarc
   chown -R anaconda:tools /opt/miniconda
+  touch "${TRACKING_DIR}/.anaconda" 
 }
 
-[[ -d /opt/envs/python37 ]] || {
+[[ -f "${TRACKING_DIR}/.env-python3" ]] || {
   echo "=== creating env Python 3.7"
-  /opt/miniconda/bin/conda create --name python37 python=3.7 anaconda
+  /opt/miniconda/bin/conda create --yes --name python37 python=3.7 anaconda
   chown -R anaconda:tools /opt/envs/python37
+  touch "${TRACKING_DIR}/.env-python3"
 }
 
-[[ -d /opt/envs/r ]] || {
+[[ -f "${TRACKING_DIR}/.env-r" ]] || {
   echo "=== creating env R"
-  /opt/miniconda/bin/conda create --name r r-essentials r-base
+  /opt/miniconda/bin/conda create --yes --name r r-essentials r-base
   chown -R anaconda:tools /opt/envs/r
+  touch "${TRACKING_DIR}/.env-r"
 }
 
 
 # h2o
-[[ -d /opt/miniconda ]] || {
+[[ -f "${TRACKING_DIR}/.h2o" ]] || {
   echo "=== installing h2o standalone"
   # https://www.h2o.ai/download/#h2o
   H2O_VERSION="3.26.0.2"
   H2O_PACKAGE="h2o-${H2O_VERSION}.zip"
   H2O_URL="http://h2o-release.s3.amazonaws.com/h2o/rel-yau/2/${H2O_PACKAGE}"
-  wget -q  "${H2O_URL}" -O ~/h2o-3.26.0.2.zip
+  wget -q  "${H2O_URL}" -O ~/${H2O_PACKAGE}
   unzip ~/${H2O_PACKAGE} -d /opt/h2o-${H2O_VERSION}
   ln -s /opt/h2o-${H2O_VERSION} /opt/h2o
+  touch "${TRACKING_DIR}/.h2o"
 }
 
 # sparkling water ( TODO spark )
-# https://s3.amazonaws.com/h2o-release/sparkling-water/spark-2.4/3.26.2-2.4/sparkling-water-3.26.2-2.4.zip
+[[ -f "${TRACKING_DIR}/.h2o-sparkling-water" ]] || {
+  echo "=== installing h2o sparkling water"
+  # https://www.h2o.ai/download/#h2o
+  H2OSW_VERSION="3.26.2-2.4"
+  H2OSW_PACKAGE="sparkling-water-${H2OSW_VERSION}.zip"
+  H2OSW_URL="https://s3.amazonaws.com/h2o-release/sparkling-water/spark-2.4/${H2OSW_VERSION}/${H2OSW_PACKAGE}"
+  wget -q  "${H2OSW_URL}" -O ~/${H2OSW_PACKAGE}
+  unzip ~/${H2OSW_PACKAGE} -d /opt/h2o-sparkling-water-${H2OSW_VERSION}
+  ln -s /opt/h2o-sparkling-water-${H2OSW_VERSION} /opt/h2o-saprkling-water
+  touch "${TRACKING_DIR}/.h2o-saprking-water"
+}
+
 
 
 echo "end of install-tools $( date )"
---
+++ utils/run-h2owebui.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# tested against  a AWS EC2 instance with a Linux 2 VM (Ubuntu)
+
+set -eu
+trap "{ echo 'h2o failed' ; exit 255; }" SIGINT SIGTERM ERR
+
+# TODO check h2o for existence
+# TODO check java for existence
+
+cd /opt/h2o
+java -jar h2o.jar
+
+
