a4dbb4a09fbc134a58bbc7c1ea787d9ea370cd44
Claude Falguiere <cfalguiere@gmail.com>
2019-08-18 19:20:46 +0200

    only useradd

--- env-setup/create-user.sh
+++ env-setup/create-user.sh
@@ -4,11 +4,4 @@
 set -eu
 trap "{ echo 'user creation failed' ; exit 255; }" SIGINT SIGTERM ERR
 
-[[ $# -ne 1 ]] && { echo "ERROR: missing parameter userid"; exit 254 }
-
-userid=$1
-
-echo "creating user $userid"
-
-echo "user $userid created"
 
