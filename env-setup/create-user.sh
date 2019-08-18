96dc20c3a713de17557454322604cc47783f4ae5
Claude Falguiere <cfalguiere@gmail.com>
2019-08-18 19:41:23 +0200

    version complete

--- env-setup/create-user.sh
+++ env-setup/create-user.sh
@@ -4,7 +4,38 @@
 set -eu
 trap "{ echo 'user creation failed' ; exit 255; }" SIGINT SIGTERM ERR
 
-[[ $# -ne 1 ]] && { echo "ERROR: missing parameter userid"; exit 254 }
+[[ $# -eq 1 ]] || {
+  echo "ERROR - missing parameter userid"
+  exit 254 
+}
+
+userid=$1
+
+echo "creating user $userid"
+
+# create user
+useradd --create-home --shell /bin/bash  --disable-password "$userid"
+usermod -aG devs "$userid"
+usermod -aG tools "$userid"
+usermod -aG sudo "$userid"
+
+# key pair
+key_dir="/home/${userid}/.ssh"
+mkdir "$key_dir"
+chmod 700 "$key_dir"
+touch "${key_dir}//authorized_keys"
+chmod 600 "${key_dir}/authorized_keys"
+cp /home/ubuntu/.ssh/authorized_keys ".${key_dir}/authorized_keys"
+
+# conda configuration
+cp env-setup/user-condarc "/home/${userid}/.condarc"
+echo 'PATH="/opt/miniconda/bin;${PATH}"' | tee -a "/home/${userid}/.bashrc"
+
+# useful variables
+echo "export LOCAL_IP=$( curl --silent http://169.254.169.254/latest/meta-data/local-ipv4 )" |  tee -a "/home/${userid}/.bashrc"
+echo "export PUBLIC_IP=$( curl --silent http://169.254.169.254/latest/meta-data/public-ipv4 )" |  tee -a "/home/${userid}/.bashrc"
+
+echo "user $userid created"
 
 
 
