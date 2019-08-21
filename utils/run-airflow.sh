38f622b0951520e44e484e5254be122ef744233c
Claude Falguiere <cfalguiere@gmail.com>
2019-08-21 22:38:49 +0200

    fixed tmux session name

--- utils/run-airflow.sh
+++ utils/run-airflow.sh
@@ -10,13 +10,13 @@ trap "{ echo 'ERROR - MLflow UI failed' ; exit 255; }" SIGINT SIGTERM ERR
 # start the web server, default port is 8080
 ! tmux has-session -t airflowws 2>/dev/null && {
   echo "=== starting airflow web server"
-  tmux new -d -s mlflowui bash -c "source /etc/profile.d/conda.sh; conda activate python36; cd /opt/airflow; airflow webserver -p 8080" &
+  tmux new -d -s airflowws bash -c "source /etc/profile.d/conda.sh; conda activate python36; cd /opt/airflow; airflow webserver -p 8080" &
 }
 
 # start the scheduler
 ! tmux has-session -t airflowscheduler 2>/dev/null && {
   echo "=== starting airflow scheduler"
-  tmux new -d -s mlflowui bash -c "source /etc/profile.d/conda.sh; conda activate python36; cd /opt/airflow; airflow scheduler" &
+  tmux new -d -s airflowscheduler bash -c "source /etc/profile.d/conda.sh; conda activate python36; cd /opt/airflow; airflow scheduler" &
 }
 
 # TODO check airflow for existence
