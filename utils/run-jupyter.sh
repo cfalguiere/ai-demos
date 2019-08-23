48aff0e33850164aa932cd7e9e6f2734a27784c7
Claude Falguiere <cfalguiere@gmail.com>
2019-08-24 00:23:30 +0200

    added no browser

--- utils/run-jupyter.sh
+++ utils/run-jupyter.sh
@@ -6,7 +6,7 @@ trap "{ echo 'ERROR - Jupyter failed' ; exit 255; }" SIGINT SIGTERM ERR
 
 ! tmux has-session -t jupyter  2>/dev/null && {
   echo "=== starting Jupyter"
-  tmux new -d -s jupyter bash -c "source /etc/profile.d/conda.sh; conda activate python36; source env-setup/setenv.sh; cd /opt/jupyter; jupyter notebook | tee ${AIDEMOS_TRACKING_DIR}/jupyter.out" &
+  tmux new -d -s jupyter bash -c "source /etc/profile.d/conda.sh; conda activate python36; source env-setup/setenv.sh; cd /opt/jupyter; jupyter notebook --no-browser | tee ${AIDEMOS_TRACKING_DIR}/jupyter.out" &
 }
 
 # TODO check for existence
