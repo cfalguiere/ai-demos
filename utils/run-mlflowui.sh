7456385918a853b963427e3c622cde12f64810b5
Claude Falguiere <cfalguiere@gmail.com>
2019-08-24 00:12:33 +0200

    unbound var

--- utils/run-mlflowui.sh
+++ utils/run-mlflowui.sh
@@ -7,7 +7,7 @@ trap "{ echo 'ERROR - MLflow UI failed' ; exit 255; }" SIGINT SIGTERM ERR
 ! tmux has-session -t mlflowui 2>/dev/null && {
   echo "=== starting MLFlow UI"
   cd /opt/mlflow
-  tmux new -d -s mlflowui bash -c "source /etc/profile.d/conda.sh; conda activate python36; cd /opt/mlflow; mlflow ui | tee ${AIDEMOS_TRACKING_DIR}/mlflow-ui.out" &
+  tmux new -d -s mlflowui bash -c "source /etc/profile.d/conda.sh; conda activate python36; source env-setup/setenv.sh; /opt/mlflow; mlflow ui | tee ${AIDEMOS_TRACKING_DIR}/mlflow-ui.out" &
 
 }
 
