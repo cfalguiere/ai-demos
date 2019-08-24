464377022e7ba13be2743fdf660e6356fe384b0a
Claude Falguiere <cfalguiere@gmail.com>
2019-08-24 22:03:08 +0200

    fixed unbound

--- utils/run-mlflowui.sh
+++ utils/run-mlflowui.sh
@@ -6,11 +6,12 @@ trap "{ echo 'ERROR - MLflow UI failed' ; exit 255; }" SIGINT SIGTERM ERR
 
 ! tmux has-session -t mlflowui 2>/dev/null && {
   echo "=== starting MLFlow UI"
-  cd /opt/mlflow
-  tmux new -d -s mlflowui bash -c "source /etc/profile.d/conda.sh; conda activate python36; source env-setup/setenv.sh; /opt/mlflow; mlflow server --host "${PUBLIC_IP}" | tee ${AIDEMOS_TRACKING_DIR}/mlflow-ui.out" &
-
+  set +u
+  tmux new -d -s mlflowui bash -c "source /etc/profile.d/conda.sh; conda activate python36; source env-setup/setenv.sh; cd /opt/mlflow; mlflow server --host ${PUBLIC_IP} | tee ${AIDEMOS_TRACKING_DIR}/mlflow-ui.out" &
+  set -u
 }
 
 # TODO check for existence
 
 echo "INFO - MLFlow UI started"
+@
