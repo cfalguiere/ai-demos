fdb15f655fa07634b213c1265f62e3d92e45438e
Claude Falguiere <cfalguiere@gmail.com>
2019-08-24 23:14:52 +0200

    fixed script path

--- utils/run-jupyter.sh
+++ utils/run-jupyter.sh
@@ -7,7 +7,7 @@ trap "{ echo 'ERROR - Jupyter failed' ; exit 255; }" SIGINT SIGTERM ERR
 ! tmux has-session -t jupyter  2>/dev/null && {
   echo "=== starting Jupyter"
   set +u
-  tmux new -d -s jupyter bash -c "source /etc/profile.d/conda.sh; conda activate python36; source env-setup/setenv.sh; cd /opt/jupyter; jupyter notebook --no-browser --ip ${PUBLIC_IP} | tee ${AIDEMOS_TRACKING_DIR}/jupyter.out" &
+  tmux new -d -s jupyter bash -c "source /etc/profile.d/conda.sh; conda activate python36; source ${AIDEMOS_REPO_DIR}/env-setup/setenv.sh; cd /opt/jupyter; jupyter notebook --no-browser --ip ${PUBLIC_IP} | tee ${AIDEMOS_TRACKING_DIR}/jupyter.out" &
   set -u
 }
 
--- utils/run-mlflowui.sh
+++ utils/run-mlflowui.sh
@@ -7,7 +7,7 @@ trap "{ echo 'ERROR - MLflow UI failed' ; exit 255; }" SIGINT SIGTERM ERR
 ! tmux has-session -t mlflowui 2>/dev/null && {
   echo "=== starting MLFlow UI"
   set +u
-  tmux new -d -s mlflowui bash -c "source /etc/profile.d/conda.sh; conda activate python36; source env-setup/setenv.sh; cd /opt/mlflow; mlflow server --host ${PUBLIC_IP} | tee ${AIDEMOS_TRACKING_DIR}/mlflow-ui.out" &
+  tmux new -d -s mlflowui bash -c "source /etc/profile.d/conda.sh; conda activate python36; source ${AIDEMOS_REPO_DIR}/env-setup/setenv.sh; cd /opt/mlflow; mlflow server --host ${PUBLIC_IP} | tee ${AIDEMOS_TRACKING_DIR}/mlflow-ui.out" &
   set -u
 }
 
