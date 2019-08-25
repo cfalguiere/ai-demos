9474ecbfbd1705e9abc605c93602ef5a68a8443d
Claude Falguiere <cfalguiere@gmail.com>
2019-08-25 17:36:30 +0200

    fixed mlflow run

--- utils/run-mlflowui.sh
+++ utils/run-mlflowui.sh
@@ -6,10 +6,11 @@ trap "{ echo 'ERROR - MLflow UI failed' ; exit 255; }" SIGINT SIGTERM ERR
 
 ! tmux has-session -t mlflowui 2>/dev/null && {
   echo "=== starting MLFlow UI"
-  cd ${AIDEMOS_REPO_DIR}
   set +u
-  tmux new -d -s mlflowui bash -c "source env-setup/setenv.sh; cd /opt/mlflow; source /etc/profile.d/conda.sh; conda activate python36; mlflow server --host ${PUBLIC_IP} | tee ${AIDEMOS_TRACKING_DIR}/mlflow-ui.out" &
+  cd ${AIDEMOS_REPO_DIR}
+  tmux new -d -s mlflowui bash -c "source env-setup/setenv.sh; cd /opt/mlflow; source /etc/profile.d/conda.sh; conda activate python36; mlflow server --backend-store-uri /opt/mlflow/mlruns  --default-artifact-root /opt/mlflow/mlruns --host ${PUBLIC_IP} | tee ${AIDEMOS_TRACKING_DIR}/mlflow-ui.out" &
   set -u
+  #TODO creation d'un espace /data ou s3
 }
 
 # TODO check for existence
