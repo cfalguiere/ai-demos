cd1a30547abae102223f6a6ca95c1bf9f33ac5e3
Claude Falguiere <cfalguiere@gmail.com>
2019-08-25 18:49:49 +0200

    removed vars and move logs to service directory

--- utils/run-jupyter.sh
+++ utils/run-jupyter.sh
@@ -6,10 +6,9 @@ trap "{ echo 'ERROR - Jupyter failed' ; exit 255; }" SIGINT SIGTERM ERR
 
 ! tmux has-session -t jupyter  2>/dev/null && {
   echo "=== starting Jupyter"
-  set +u
-  cd ${AIDEMOS_REPO_DIR}
-  tmux new -d -s jupyter bash -c 'source env-setup/setenv.sh; cd /opt/jupyter; source /etc/profile.d/conda.sh; conda activate python36; jupyter notebook --no-browser --ip ${PUBLIC_IP} | tee ${AIDEMOS_TRACKING_DIR}/jupyter.out' &
-  set -u
+  tmux new -d -s jupyter bash -c 'source env-setup/setenv.sh; cd /opt/jupyter; source /etc/profile.d/conda.sh; conda activate python36; jupyter notebook --no-browser --ip 0.0.0.0 | tee /opt/jupyter/jupyter.out' &
+  # show token
+  tmux new -d -s jupyter bash -c 'source env-setup/setenv.sh; cd /opt/jupyter; source /etc/profile.d/conda.sh; conda activate python36; jupyter notebook list'
 }
 
 # TODO check for existence
