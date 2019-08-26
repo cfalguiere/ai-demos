8f1a6e98d49fddd9ecb9463ae00e753010933fe6
Claude Falguiere <cfalguiere@gmail.com>
2019-08-26 22:21:29 +0200

    fixed awk program

--- env-setup/install-tools.sh
+++ env-setup/install-tools.sh
@@ -119,9 +119,9 @@ sudo -H -u mlflow /opt/mlflow/mlflowctl start
 sudo -H -u airflow /opt/airflow/airflowctl start
 sudo -H -u jupyter /opt/jupyter/jupyterctl start
 
-sleep 2
+sleep 4
 export AIDEMOS_TOKEN=$( awk 'BEGIN {FS="="};/token/ {print $2}'  /var/log/aidemos/jupyter/jupyter.out | head -1 )
-
+echo "AIDEMOS_TOKEN=$AIDEMOS_TOKEN"
 
 
 [[ -f "${AIDEMOS_TRACKING_DIR}/.web" ]] || {
