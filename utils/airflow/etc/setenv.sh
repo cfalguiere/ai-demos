e3440338bcf14e684de00caa1f202d36f7bb68fb
Claude Falguiere <cfalguiere@gmail.com>
2019-08-29 19:56:48 +0200

    removed ;

--- utils/airflow/etc/setenv.sh
+++ utils/airflow/etc/setenv.sh
@@ -2,7 +2,7 @@
 set -eu
 trap "{ echo 'ERROR - airflow setenv FAILED' ; exit 255; }" SIGINT SIGTERM ERR
 
-source /etc/profile.d/conda.sh;
-conda activate python36;
+source /etc/profile.d/conda.sh
+conda activate python36
 
 echo "INFO - airflow setenv COMPLETED"
--- utils/jupyter/etc/setenv.sh
+++ utils/jupyter/etc/setenv.sh
@@ -2,7 +2,7 @@
 set -eu
 trap "{ echo 'ERROR - jupyter setenv FAILED' ; exit 255; }" SIGINT SIGTERM ERR
 
-source /etc/profile.d/conda.sh;
-conda activate python36;
+source /etc/profile.d/conda.sh
+conda activate python36
 
 echo "INFO - jupyter etenv COMPLETED"
