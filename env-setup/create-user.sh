a9021183d5305cce2fc83a15f0694fab67375693
Claude Falguiere <cfalguiere@gmail.com>
2019-08-29 19:54:03 +0200

    fixed typo

--- env-setup/create-user.sh
+++ env-setup/create-user.sh
@@ -6,7 +6,7 @@ trap "{ echo 'ERROR - user creation failed' ; exit 255; }" SIGINT SIGTERM ERR
 
 [[ $# -eq 1 ]] || {
   echo "ERROR - missing parameter userid in user creation"
-  exit 254 
+  exit 254
 }
 
 userid=$1
@@ -33,7 +33,7 @@ chown -R "${userid}:${userid}"  "$key_dir"
 # conda configuration
 cp env-setup/user-condarc "/home/${userid}/.condarc"
 #echo 'export PATH="/opt/miniconda/bin;${PATH}"' | tee -a "/home/${userid}/.bashrc"
-echo "source . /etc/profile.d/conda.sh" | tee -a "/home/${userid}/.bashrc"
+echo "source /etc/profile.d/conda.sh" | tee -a "/home/${userid}/.bashrc"
 
 # useful variables
 echo "export LOCAL_IP=$( curl --silent http://169.254.169.254/latest/meta-data/local-ipv4 )" |  tee -a "/home/${userid}/.bashrc"
@@ -42,6 +42,3 @@ echo "export PUBLIC_IP=$( curl --silent http://169.254.169.254/latest/meta-data/
 id $userid
 
 echo "INFO - user $userid created"
-
-
-
