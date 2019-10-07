3b8ae703f42df1dfa2f239a317e2e480ec093870
Claude Falguiere <cfalguiere@gmail.com>
2019-10-07 22:12:59 +0200

    ajout echos

--- install/mlflow/install.sh
+++ install/mlflow/install.sh
@@ -1,17 +1,18 @@
 BASEDIR=$(readlink -f $0 | xargs dirname)
 
 # anaconda
+echo "INFO - install Anaconda"
 ANACONDA_VERSION="Anaconda3-2019.07-Linux-x86_64.sh"
 ANACONDA_URL="https://repo.anaconda.com/archive/${ANACONDA_VERSION}"
 wget -q  "${ANACONDA_URL}"  -O miniconda.sh
-mkdir /opt
+mkdir -p /opt
+cd $_
 bash ./miniconda.sh -b -p /opt/miniconda
 /opt/miniconda/bin/conda update -y conda
 
 # mlflow
+echo "INFO - install MLFlow"
 /opt/miniconda/bin/conda env create --file ${BASEDIR}/conda-mlflow.yml
+mkdir -p /opt/mlflow/mlruns
 
-mkdir ./mlruns
-
-# utils
-snap install tree
+echo "INFO - install Completed"
